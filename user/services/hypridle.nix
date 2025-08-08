8372df040a19af627455379490ff94b7f6e217c5
januarpancaran <januar352@gmail.com>
2025-08-08 16:09:08 +0700

    adding hypridle

---
+++ user/services/hypridle.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+
+{
+  services.hypridle = {
+    package = pkgs.hypridle;
+    enable = true;
+
+    settings = 
+    let
+      monitor = "eDP-1";
+    in
+    {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "niri msg output ${monitor} on";
+      };
+
+      listener = [
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+        }
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330;
+          on-timeout = "niri msg output ${monitor} off";
+          on-resume = "niri msg output ${monitor} on";
+        }
+        {
+          timeout = 600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+}
--- user/wm/niri/autostart/default.nix
+++ user/wm/niri/autostart/default.nix
@@ -5,7 +5,6 @@
     }) [
       "eww daemon"
       "eww open bar"
-      "hypridle"
       "gsettings set org.gnome.desktop.interface gtk-theme Dracula"
       "gsettings set org.gnome.desktop.interface icon-theme Tela-nord-dark"
       "gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Classic"
