9a2b7c669b8076cb71ed6040024ecc21fc7e3156
januarpancaran <januar352@gmail.com>
2025-08-08 14:35:44 +0700

    add: adding mako

---
+++ user/services/mako/default.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+
+{
+  services.mako = {
+    package = pkgs.mako;
+    enable = true;
+
+    settings = {
+      sort = "-time";
+      layer = "overlay";
+      background-color = "#2e3440";
+      width = 400;
+      height = 110;
+      border-size = 2;
+      border-color = "#88c0d0";
+      border-radius = 12;
+      progress-color = "#cba6f7";
+      icons = 1;
+      max-icon-size = 72;
+      default-timeout = 5000;
+      ignore-timeout = 1;
+      font = "JetBrainsMono Nerd Font 14";
+      icon-path = "~/.config/mako/icons/";
+
+      "urgency=low" = {
+        border-color = "#cccccc";
+      };
+
+      "urgency=normal" = {
+        border-color = "#d08770";
+      };
+
+      "urgency=high" = {
+        border-color = "#bf616a";
+        default-timeout = 0;
+      };
+    };
+  };
+
+  home.file.".config/mako/icons" = {
+    source = ./icons;
+    recursive = true;
+  };
+}
