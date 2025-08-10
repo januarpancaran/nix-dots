4729ac1f8fa59e7418cd5095db181750a9eacb3a
januarpancaran <151476664+januarpancaran@users.noreply.github.com>
2025-08-10 14:20:39 +0000

    format: format files

--- system/udev/default.nix
+++ system/udev/default.nix
@@ -1,6 +1,8 @@
-{pkgs, userSettings, ...}:
-
 {
+  pkgs,
+  userSettings,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     acpi
   ];
@@ -26,8 +28,7 @@
         notify-send "Discharging" "$BATTERY_LEVEL% of battery remaining." -u low -i "$ICON_DIR/battery.png" -t 5000 -r 9991
       fi
     '';
-  in
-  ''
+  in ''
     ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{type}=="Mains", ATTRS{online}=="1", ENV{WAYLAND_DISPLAY}="${waylandDisplay}", ENV{DBUS_SESSION_BUS_ADDRESS}="${dbus}" RUN+="${pkgs.su}/bin/su ${userSettings.username} -c '${chargingNotifier}/bin/charging-notifier 1'"
     ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{type}=="Mains", ATTRS{online}=="0", ENV{WAYLAND_DISPLAY}="${waylandDisplay}", ENV{DBUS_SESSION_BUS_ADDRESS}="${dbus}" RUN+="${pkgs.su}/bin/su ${userSettings.username} -c '${chargingNotifier}/bin/charging-notifier 0'"
   '';
