df89c9c080dc63da2e95c302185b0cee1607e43c
januarpancaran <januar352@gmail.com>
2025-08-17 23:03:56 +0700

    change: changing default outputs scale to 1.25

--- user/wm/niri/outputs/default.nix
+++ user/wm/niri/outputs/default.nix
@@ -1,11 +1,11 @@
 {
   programs.niri.settings.outputs = {
     "eDP-1" = {
-      scale = 1;
+      scale = 1.25;
     };
 
     "HDMI-A-1" = {
-      scale = 1;
+      scale = 1.25;
     };
   };
 }
