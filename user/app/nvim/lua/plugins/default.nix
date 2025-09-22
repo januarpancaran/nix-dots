fc3d5eca5ab5e9cb6cbc637d6ee4cd803835e2c8
januarpancaran <januar352@gmail.com>
2025-09-22 19:32:12 +0700

    feat: adding alpha-nvim and comment-nvim

--- user/app/nvim/lua/plugins/default.nix
+++ user/app/nvim/lua/plugins/default.nix
@@ -6,6 +6,18 @@
   programs.neovim.plugins = with pkgs.vimPlugins; let
     read = file: "${builtins.readFile file}";
   in [
+    {
+      plugin = alpha-nvim;
+      type = "lua";
+      config = read ../../after/plugins/alpha-nvim.lua;
+    }
+
+    {
+      plugin = comment-nvim;
+      type = "lua";
+      config = read ../../after/plugins/comment-nvim.lua;
+    }
+
     # lsp
     {
       plugin = nvim-lspconfig;
