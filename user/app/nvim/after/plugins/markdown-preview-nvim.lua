1a4968d63a0c2444869da5d1a550c70ae60052c9
januarpancaran <151476664+januarpancaran@users.noreply.github.com>
2025-09-22 14:07:42 +0000

    format: format files

--- user/app/nvim/after/plugins/markdown-preview-nvim.lua
+++ user/app/nvim/after/plugins/markdown-preview-nvim.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end
+	pattern = "markdown",
+	callback = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
 })
