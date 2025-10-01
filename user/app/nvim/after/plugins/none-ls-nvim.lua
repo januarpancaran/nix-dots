d33d6bc02f2bbdd258a082275f4e6276cff83b57
januarpancaran <151476664+januarpancaran@users.noreply.github.com>
2025-10-01 05:47:38 +0000

    format: format files

--- user/app/nvim/after/plugins/none-ls-nvim.lua
+++ user/app/nvim/after/plugins/none-ls-nvim.lua
@@ -2,54 +2,54 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local none_ls_on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
-  end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end
 end
 
 null_ls.setup({
-  sources = {
-    -- Bash
-    null_ls.builtins.formatting.shfmt,
+	sources = {
+		-- Bash
+		null_ls.builtins.formatting.shfmt,
 
-    -- C / C++
-    null_ls.builtins.formatting.clang_format,
+		-- C / C++
+		null_ls.builtins.formatting.clang_format,
 
-    -- CSS/Markdown/TS/Svelte
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.prettier,
+		-- CSS/Markdown/TS/Svelte
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.prettier,
 
-    -- Go
-    null_ls.builtins.formatting.gofumpt,
+		-- Go
+		null_ls.builtins.formatting.gofumpt,
 
-    -- Java
-    null_ls.builtins.formatting.google_java_format,
+		-- Java
+		null_ls.builtins.formatting.google_java_format,
 
-    -- Lua
-    null_ls.builtins.formatting.stylua,
+		-- Lua
+		null_ls.builtins.formatting.stylua,
 
-    -- Nix
-    null_ls.builtins.formatting.alejandra,
+		-- Nix
+		null_ls.builtins.formatting.alejandra,
 
-    -- PHP
-    null_ls.builtins.formatting.phpcsfixer,
+		-- PHP
+		null_ls.builtins.formatting.phpcsfixer,
 
-    -- Python
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort,
+		-- Python
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
 
-    -- Rust
-    require("none-ls.formatting.rustfmt"),
+		-- Rust
+		require("none-ls.formatting.rustfmt"),
 
-    -- SQL
-    null_ls.builtins.formatting.sqlfluff,
-  },
-  on_attach = none_ls_on_attach,
+		-- SQL
+		null_ls.builtins.formatting.sqlfluff,
+	},
+	on_attach = none_ls_on_attach,
 })
