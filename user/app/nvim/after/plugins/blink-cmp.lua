343765ec304229ac4b2bba55f42ddd5f3bf822cb
januarpancaran <januar352@gmail.com>
2025-10-01 20:11:07 +0700

    chore(nvim): disabling copilot completion as default

--- user/app/nvim/after/plugins/blink-cmp.lua
+++ user/app/nvim/after/plugins/blink-cmp.lua
@@ -65,24 +65,25 @@ require("blink.cmp").setup({
 	},
 
 	sources = {
-		default = { "lsp", "path", "snippets", "buffer", "copilot" },
-		providers = {
-			copilot = {
-				name = "copilot",
-				module = "blink-cmp-copilot",
-				score_offset = 100,
-				async = true,
-				transform_items = function(_, items)
-					local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-					local kind_idx = #CompletionItemKind + 1
-					CompletionItemKind[kind_idx] = "Copilot"
-					for _, item in ipairs(items) do
-						item.kind = kind_idx
-					end
-					return items
-				end,
-			},
-		},
+		default = { "lsp", "path", "snippets", "buffer" },
+		-- default = { "lsp", "path", "snippets", "buffer", "copilot" },
+		-- providers = {
+		-- 	copilot = {
+		-- 		name = "copilot",
+		-- 		module = "blink-cmp-copilot",
+		-- 		score_offset = 100,
+		-- 		async = true,
+		-- 		transform_items = function(_, items)
+		-- 			local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+		-- 			local kind_idx = #CompletionItemKind + 1
+		-- 			CompletionItemKind[kind_idx] = "Copilot"
+		-- 			for _, item in ipairs(items) do
+		-- 				item.kind = kind_idx
+		-- 			end
+		-- 			return items
+		-- 		end,
+		-- 	},
+		-- },
 	},
 
 	fuzzy = { implementation = "prefer_rust_with_warning" },
