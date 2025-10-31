0baee5953ef2db58d7126cf310e319ef5dcac9c4
januarpancaran <januar352@gmail.com>
2025-10-31 13:42:40 +0700

    refactor: changing fcitx5-chinese-addons to qt6Packages.fcitx5-chinese-addons

--- user/inputs/fcitx5.nix
+++ user/inputs/fcitx5.nix
@@ -10,7 +10,7 @@
         addons = with pkgs; [
           fcitx5-gtk
           fcitx5-nord
-          fcitx5-chinese-addons
+          qt6Packages.fcitx5-chinese-addons
           fcitx5-anthy
           fcitx5-hangul
         ];
