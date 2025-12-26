bbe733db0c571b44879bae6a59a9a0ce3913eb55
januarpancaran <januar352@gmail.com>
2025-12-26 21:08:03 +0700

    fix: fixing nvim lspconfig

--- user/app/nvim/after/plugins/lspconfig-nvim.lua
+++ user/app/nvim/after/plugins/lspconfig-nvim.lua
@@ -1,62 +1,35 @@
 if not vim.g.vscode then
-	local lsp_on_attach = function(_, bufnr)
-		vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-	end
-
-	if vim.lsp.inlay_hint then
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(args)
-				local client = vim.lsp.get_client_by_id(args.data.client_id)
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-				end
-			end,
-		})
-	end
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 	local servers = {
-		bashls = {},
-		clangd = {},
-		cmake = {},
-		csharp_ls = {},
-		cssls = {},
-		gopls = {},
-		html = {},
-		jdtls = {},
-		lua_ls = {
-			settings = {
-				Lua = {
-					diagnostics = { globals = { "vim" } },
-				},
-			},
-		},
-		marksman = {},
-		nixd = {
-			settings = {
-				nixd = {
-					formatting = { command = { "alejandra" } },
-				},
-			},
-		},
-		intelephense = {},
-		pyright = {},
-		rust_analyzer = {},
-		sqls = {},
-		svelte = {},
-		tailwindcss = {},
-		terraformls = {},
-		ts_ls = {},
-		yamlls = {},
+		"bashls",
+		"clangd",
+		"cmake",
+		"csharp_ls",
+		"cssls",
+		"gopls",
+		"html",
+		"jdtls",
+		"jsonls",
+		"lua_ls",
+		"marksman",
+		"nixd",
+		"intelephense",
+		"pyright",
+		"rust_analyzer",
+		"sqls",
+		"svelte",
+		"tailwindcss",
+		"terraformls",
+		"ts_ls",
+		"yamlls",
 	}
 
-	for server, config in pairs(servers) do
-		local lsp = vim.lsp.config[server]
-
-		if lsp and lsp.setup then
-			lsp.setup(vim.tbl_extend("force", {
-				on_attach = lsp_on_attach,
-				capabilities = vim.lsp.protocol.make_client_capabilities(),
-			}, config))
-		end
+	for _, server in ipairs(servers) do
+		vim.lsp.config[server] = {
+			capabilities = capabilities,
+		}
 	end
+
+	vim.lsp.enable(servers)
 end
