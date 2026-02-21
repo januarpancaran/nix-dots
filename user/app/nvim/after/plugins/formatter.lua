4a6bf9db4a875d2606a8a54d8f01d9da99ec0497
januarpancaran <151476664+januarpancaran@users.noreply.github.com>
2026-02-21 22:46:43 +0000

    refactor: format files

--- user/app/nvim/after/plugins/formatter.lua
+++ user/app/nvim/after/plugins/formatter.lua
@@ -1,61 +1,61 @@
 if not vim.g.vscode then
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	local null_ls = require("null-ls")
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    local on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end
+	local on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end
 
-    null_ls.setup({
-        sources = {
-            -- bash
-            null_ls.builtins.formatting.shfmt,
+	null_ls.setup({
+		sources = {
+			-- bash
+			null_ls.builtins.formatting.shfmt,
 
-            -- c/cpp
-            null_ls.builtins.formatting.clang_format,
+			-- c/cpp
+			null_ls.builtins.formatting.clang_format,
 
-            -- csharp
-            null_ls.builtins.formatting.csharpier,
+			-- csharp
+			null_ls.builtins.formatting.csharpier,
 
-            -- css/markdown/typescript
-            null_ls.builtins.formatting.prettierd,
+			-- css/markdown/typescript
+			null_ls.builtins.formatting.prettierd,
 
-            -- go
-            null_ls.builtins.formatting.gofumpt,
+			-- go
+			null_ls.builtins.formatting.gofumpt,
 
-            -- java
-            null_ls.builtins.formatting.google_java_format,
+			-- java
+			null_ls.builtins.formatting.google_java_format,
 
-            -- lua
-            null_ls.builtins.formatting.stylua,
+			-- lua
+			null_ls.builtins.formatting.stylua,
 
-            -- nix
-            null_ls.builtins.formatting.nixfmt,
+			-- nix
+			null_ls.builtins.formatting.nixfmt,
 
-            -- php
-            null_ls.builtins.formatting.phpcsfixer,
-            null_ls.builtins.formatting.blade_formatter,
+			-- php
+			null_ls.builtins.formatting.phpcsfixer,
+			null_ls.builtins.formatting.blade_formatter,
 
-            -- python
-            null_ls.builtins.formatting.black,
-            null_ls.builtins.formatting.isort,
+			-- python
+			null_ls.builtins.formatting.black,
+			null_ls.builtins.formatting.isort,
 
-            -- rust
-            require("none-ls.formatting.rustfmt"),
+			-- rust
+			require("none-ls.formatting.rustfmt"),
 
-            -- xml
-            null_ls.builtins.formatting.xmllint,
-        },
+			-- xml
+			null_ls.builtins.formatting.xmllint,
+		},
 
-        on_attach = on_attach,
-    })
+		on_attach = on_attach,
+	})
 end
