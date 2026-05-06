d15303a5a8f630d5ec11a763f9ce5c1ff5d06ec4
Januar Pancaran <januar352@gmail.com>
2026-05-06 16:20:04 +0700

    feat!: removing nix configured firefox

--- user/app/browser/firefox/default.nix
+++ user/app/browser/firefox/default.nix
@@ -1,11 +1,7 @@
 { config, pkgs, ... }:
 {
-  imports = [
-    ./policies.nix
-    ./profiles.nix
-  ];
-
   programs.firefox = {
+    package = pkgs.firefox;
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
   };
