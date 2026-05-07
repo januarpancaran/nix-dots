cb20f1c29cbb7fb4704497e9af9ee4f285a48259
Januar Pancaran <januar352@gmail.com>
2026-05-07 08:08:32 +0700

    feat: exposing common libraries for python project and removing nix configured python packages

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -75,8 +75,6 @@
     ++ lib.optionals userSettings.enableProgrammingPkgs (
       with pkgs;
       [
-        opencode
-
         bun
         cargo
         dotnet-sdk_10
@@ -94,32 +92,17 @@
         nixd
         nssTools
         nodejs
+        opencode
         openssl
         php
         php84Packages.composer
         pkg-config
+        python3
         ruby_4_0
         rustc
         spring-boot-cli
+        uv
         zlib
-
-        (python3.withPackages (
-          ps: with ps; [
-            numpy
-            pandas
-            matplotlib
-            seaborn
-            scikit-learn
-            streamlit
-            fastapi
-            pydantic
-            uvicorn
-            flask
-            flask-cors
-            django
-            django-cors-headers
-          ]
-        ))
       ]
     );
 
--- system/env/default.nix
+++ system/env/default.nix
@@ -1,4 +1,6 @@
 {
+    lib,
+    pkgs,
   systemSettings,
   userSettings,
   ...
@@ -19,6 +21,28 @@
     MOZ_ENABLE_WAYLAND = 1;
     NIXOS_OZONE_WL = 1;
     NH_FLAKE = systemSettings.flakeDir;
+
+    LD_LIBRARY_PATH = lib.mkForce (lib.makeLibraryPath (with pkgs; [
+    stdenv.cc.cc.lib  # libstdc++ - for anything with C++ extensions
+    libz              # libz - compression, used by numpy, pillow, etc
+    pipewire          # audio
+    
+    # Common for data science / ML
+    libGL             # OpenGL - opencv, matplotlib
+    glib              # libglib - many packages depend on this
+    
+    # Common for web / networking
+    openssl           # SSL - requests, httpx, cryptography
+    curl              # libcurl
+    
+    # Common for image processing
+    libjpeg           # Pillow
+    libpng            # Pillow
+    libwebp           # Pillow
+    
+    # Common for database
+    sqlite            # most projects use sqlite
+    ]));
   };
 
   environment.pathsToLink = [
