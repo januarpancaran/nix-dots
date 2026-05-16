7413e8c806e9b6eb0c85aaa35f105fcc6fae83a6
Januar Pancaran <januar352@gmail.com>
2026-05-16 23:04:24 +0700

    feat: add nix-ld configuration and libraries

--- system/env/default.nix
+++ system/env/default.nix
@@ -1,6 +1,4 @@
 {
-  lib,
-  pkgs,
   systemSettings,
   userSettings,
   ...
@@ -21,33 +19,6 @@
     MOZ_ENABLE_WAYLAND = 1;
     NIXOS_OZONE_WL = 1;
     NH_FLAKE = systemSettings.flakeDir;
-
-    LD_LIBRARY_PATH = lib.mkForce (
-      lib.makeLibraryPath (
-        with pkgs;
-        [
-          stdenv.cc.cc.lib # libstdc++ - for anything with C++ extensions
-          zlib # libz - compression, used by numpy, pillow, etc
-          pipewire # audio
-
-          # Common for data science / ML
-          libGL # OpenGL - opencv, matplotlib
-          glib # libglib - many packages depend on this
-
-          # Common for web / networking
-          openssl # SSL - requests, httpx, cryptography
-          curl # libcurl
-
-          # Common for image processing
-          libjpeg # Pillow
-          libpng # Pillow
-          libwebp # Pillow
-
-          # Common for database
-          sqlite # most projects use sqlite
-        ]
-      )
-    );
   };
 
   environment.pathsToLink = [
--- system/services/pipewire.nix
+++ system/services/pipewire.nix
@@ -7,7 +7,6 @@
       support32Bit = true;
     };
     pulse.enable = true;
-    jack.enable = true;
     wireplumber.enable = true;
   };
 }
