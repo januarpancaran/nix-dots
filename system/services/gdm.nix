0956f66cf1d9a2dfee3737f8df011db05b73fee8
Januar Pancaran <januar352@gmail.com>
2026-06-01 09:33:35 +0700

    feat: enable auto-login for GDM with user settings

--- system/services/gdm.nix
+++ system/services/gdm.nix
@@ -1,8 +1,13 @@
+{ userSettings, ... }:
 {
   services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = userSettings.username;
+    };
+
     gdm = {
       enable = true;
-      wayland = true;
     };
 
     defaultSession = "niri";
