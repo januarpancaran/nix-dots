74345703b9745976657e98277e3afeb49f6014c1
Januar Pancaran <januar352@gmail.com>
2026-07-20 17:23:14 +0700

    feat: adding ipv6 for dns settings

--- system/services/systemd-resolved.nix
+++ system/services/systemd-resolved.nix
@@ -4,12 +4,16 @@
     settings = {
       Resolve = {
         DNS = [
-          "1.1.1.1#cloudflare-dns.com"
-          "1.0.0.1#cloudflare-dns.com"
+          "1.1.1.1"
+          "1.0.0.1"
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
         ];
         FallbackDNS = [
           "8.8.8.8"
           "8.8.4.4"
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
         ];
         DNSOverTLS = "yes";
         DNSSEC = "yes";
