998c46a2c89a1cc18d554b906e5cdd7eca6dfd3b
Januar Pancaran <januar352@gmail.com>
2026-08-31 10:41:07 +0700

    feat: adding pgvector

--- system/services/postgresql.nix
+++ system/services/postgresql.nix
@@ -8,5 +8,8 @@ lib.mkIf systemSettings.enablePostgreSQL {
   services.postgresql = {
     package = pkgs.postgresql;
     enable = true;
+    extensions = with pkgs.postgresqlPackages; [
+      pgvector
+    ];
   };
 }
