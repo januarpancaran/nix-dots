93debfecbe220d6a7c2e2e77af831bcdc7361fbf
Januar Pancaran <januar352@gmail.com>
2026-09-16 13:00:50 +0700

    feat!: removing manually installing dotnet dev cert to firefox

--- user/app/browser/firefox/default.nix
+++ user/app/browser/firefox/default.nix
@@ -10,10 +10,6 @@ lib.mkIf (builtins.elem "firefox" userSettings.enableBrowsers) {
     package = pkgs.firefox;
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
-    policies = {
-      Certificates = {
-        Install = [ "/etc/ssl/certs/aspnet-dev-${config.home.username}-ca.pem" ];
-      };
-    };
+
   };
 }
